f6f7fb906beef2cbb84226e54ef4bf49dc1446a4
Marcys06 <marcelcieslik6@gmail.com>
2026-08-27 11:36:39 +0200

    Add project files.

---
+++ RailDispatchMono/RailDispatchMono.Core/Game/FaceDirection.cs
@@ -0,0 +1,11 @@
+﻿namespace RailDispatchMono.Core
+{
+    /// <summary>
+    /// Facing direction along the X axis.
+    /// </summary>
+    internal enum FaceDirection
+    {
+        Left = -1,
+        Right = 1,
+    }
+}
---
+++ RailDispatchMono/RailDispatchMono.Core/Game/PlayerMode.cs
@@ -0,0 +1,11 @@
+﻿namespace RailDispatchMono.Core
+{
+    /// <summary>
+    /// The various modes the player could be in.
+    /// </summary>
+    internal enum PlayerMode
+    {
+        Scripting,
+        Playing
+    }
+}
