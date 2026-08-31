00835302eea9b0b1c92d29fc99486f25d55ddfa6
Marcys06 <marcelcieslik6@gmail.com>
2026-08-31 14:43:29 +0200

    0.0.15c: move Save/Load actions into pause menu

--- RailDispatchMono/RailDispatchMono.Core/Screens/PauseScreen.cs
+++ RailDispatchMono/RailDispatchMono.Core/Screens/PauseScreen.cs
@@ -1,4 +1,5 @@
 using Microsoft.Xna.Framework;
+using RailDispatchMono.Core.Game.Save;
 using RailDispatchMono.Core.Localization;
 using System;
 
@@ -8,23 +9,29 @@ namespace RailDispatchMono.Core.Screens
     {
         public event EventHandler? OnQuit;
         public event EventHandler? OnResume;
+        public event EventHandler? OnSave;
+        public event EventHandler? OnLoad;
 
-        public PauseScreen() : base(Resources.Paused)
+        public PauseScreen(bool canLoad = true) : base(Resources.Paused)
         {
             IsPopup = true;
             TransitionOnTime = TimeSpan.FromSeconds(0.2);
             TransitionOffTime = TimeSpan.FromSeconds(0.2);
 
-            MenuEntry resumeGameMenuEntry = new MenuEntry(Resources.Resume);
+            MenuEntry resumeGameMenuEntry = new MenuEntry("WZNÓW GRĘ");
+            MenuEntry saveGameMenuEntry = new MenuEntry("ZAPISZ GRĘ");
+            MenuEntry loadGameMenuEntry = new MenuEntry("WCZYTAJ GRĘ") { Enabled = canLoad };
             MenuEntry quitGameMenuEntry = new MenuEntry(Resources.Quit);
 
             resumeGameMenuEntry.Selected += ResumeGameEntrySelected;
+            saveGameMenuEntry.Selected += SaveGameEntrySelected;
+            loadGameMenuEntry.Selected += LoadGameEntrySelected;
             quitGameMenuEntry.Selected += QuitGameMenuEntrySelected;
 
             MenuEntries.Add(resumeGameMenuEntry);
+            MenuEntries.Add(saveGameMenuEntry);
+            MenuEntries.Add(loadGameMenuEntry);
             MenuEntries.Add(quitGameMenuEntry);
-
-            DebugManager.Log($"[PAUSE] Konstruktor - Liczba entries: {MenuEntries.Count}");
         }
 
         private void ResumeGameEntrySelected(object? sender, PlayerIndexEventArgs e)
@@ -33,14 +40,22 @@ namespace RailDispatchMono.Core.Screens
             ExitScreen();
         }
 
+        private void SaveGameEntrySelected(object? sender, PlayerIndexEventArgs e)
+        {
+            OnSave?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void LoadGameEntrySelected(object? sender, PlayerIndexEventArgs e)
+        {
+            OnLoad?.Invoke(this, EventArgs.Empty);
+        }
+
         private void QuitGameMenuEntrySelected(object? sender, PlayerIndexEventArgs e)
         {
             string message = Resources.QuitQuestion;
-
             MessageBoxScreen confirmQuitMessageBox = new MessageBoxScreen(message);
             confirmQuitMessageBox.Accepted += ConfirmQuitMessageBoxAccepted;
             confirmQuitMessageBox.Cancelled += ConfirmQuitMessageBoxCancelled;
-
             ScreenManager.AddScreen(confirmQuitMessageBox, ControllingPlayer);
         }
 
@@ -50,9 +65,7 @@ namespace RailDispatchMono.Core.Screens
             ScreenManager.Game.Exit();
         }
 
-        private void ConfirmQuitMessageBoxCancelled(object? sender, PlayerIndexEventArgs e)
-        {
-        }
+        private void ConfirmQuitMessageBoxCancelled(object? sender, PlayerIndexEventArgs e) { }
 
         protected override void OnCancel(PlayerIndex playerIndex)
         {
@@ -60,9 +73,6 @@ namespace RailDispatchMono.Core.Screens
             ExitScreen();
         }
 
-        public void Cancel(PlayerIndex playerIndex)
-        {
-            OnCancel(playerIndex);
-        }
+        public void Cancel(PlayerIndex playerIndex) => OnCancel(playerIndex);
     }
 }
