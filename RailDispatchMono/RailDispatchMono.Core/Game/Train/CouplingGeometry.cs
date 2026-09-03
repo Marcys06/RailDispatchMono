fd3e3c047a027cbba64b3ce6b2873f6c6bb1dc72
Marcys06 <marcelcieslik6@gmail.com>
2026-09-03 13:51:15 +0200

    feat(coupling): add vehicle endpoint geometry

---
+++ RailDispatchMono/RailDispatchMono.Core/Game/Train/CouplingGeometry.cs
@@ -0,0 +1,46 @@
+using Microsoft.Xna.Framework;
+using System;
+
+namespace RailDispatchMono.Core.Game.Train;
+
+public static class CouplingGeometry
+{
+    public const float DefaultCouplingDistance = 0.12f;
+    public const float DefaultAlignmentDot = 0.94f;
+
+    public static Vector2 GetEndpoint(Train train, int vehicleIndex, VehicleEnd end)
+    {
+        var transform = train.GetVehicleTransform(vehicleIndex);
+        Vector2 forward = new(MathF.Cos(transform.Rotation), MathF.Sin(transform.Rotation));
+        if (train.Composition.Vehicles[vehicleIndex].Orientation == VehicleOrientation.Reverse)
+            forward = -forward;
+
+        return transform.Position + (end == VehicleEnd.Front ? forward : -forward) *
+            (train.Composition.Vehicles[vehicleIndex].Parameters.Length * 0.5f);
+    }
+
+    public static Vector2 GetOutwardDirection(Train train, int vehicleIndex, VehicleEnd end)
+    {
+        var transform = train.GetVehicleTransform(vehicleIndex);
+        Vector2 forward = new(MathF.Cos(transform.Rotation), MathF.Sin(transform.Rotation));
+        if (train.Composition.Vehicles[vehicleIndex].Orientation == VehicleOrientation.Reverse)
+            forward = -forward;
+
+        return end == VehicleEnd.Front ? forward : -forward;
+    }
+
+    public static bool AreFacing(Train firstTrain, int firstIndex, VehicleEnd firstEnd,
+        Train secondTrain, int secondIndex, VehicleEnd secondEnd,
+        float minimumDot = DefaultAlignmentDot)
+    {
+        Vector2 firstToSecond = GetEndpoint(secondTrain, secondIndex, secondEnd) -
+                                GetEndpoint(firstTrain, firstIndex, firstEnd);
+        if (firstToSecond.LengthSquared() < 0.000001f)
+            return true;
+
+        firstToSecond.Normalize();
+        Vector2 secondToFirst = -firstToSecond;
+        return Vector2.Dot(GetOutwardDirection(firstTrain, firstIndex, firstEnd), firstToSecond) >= minimumDot &&
+               Vector2.Dot(GetOutwardDirection(secondTrain, secondIndex, secondEnd), secondToFirst) >= minimumDot;
+    }
+}
