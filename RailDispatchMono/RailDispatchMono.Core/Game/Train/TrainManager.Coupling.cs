eb40cef87dae8014183aef4674887c4826c92705
Marcys06 <marcelcieslik6@gmail.com>
2026-09-03 14:17:14 +0200

    0.1.5e fix SignalAspect namespace import

--- RailDispatchMono/RailDispatchMono.Core/Game/Train/TrainManager.Coupling.cs
+++ RailDispatchMono/RailDispatchMono.Core/Game/Train/TrainManager.Coupling.cs
@@ -1,6 +1,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using RailDispatchMono.Core.Game.Debug;
+using RailDispatchMono.Core.Game.Railway;
 using System;
 using System.Collections.Generic;
 
