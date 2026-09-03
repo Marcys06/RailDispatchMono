190e91b3f9f993b13ff4843eae95689b9d4f23d9
Marcys06 <marcelcieslik6@gmail.com>
2026-09-03 13:50:52 +0200

    feat(coupling): add vehicle-end model

---
+++ RailDispatchMono/RailDispatchMono.Core/Game/Train/VehicleEnd.cs
@@ -0,0 +1,8 @@
+namespace RailDispatchMono.Core.Game.Train;
+
+/// <summary>Physical end of a vehicle. This is intrinsic to the vehicle, not its travel direction.</summary>
+public enum VehicleEnd
+{
+    Front,
+    Rear
+}
