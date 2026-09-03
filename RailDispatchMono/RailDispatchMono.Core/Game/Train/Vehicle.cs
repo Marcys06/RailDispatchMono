3bc1083c04d5aee6293680fa36e8c36386271d76
Marcys06 <marcelcieslik6@gmail.com>
2026-09-03 12:41:57 +0200

    0.1.4i: expose static vehicle coupling specification

--- RailDispatchMono/RailDispatchMono.Core/Game/Train/Vehicle.cs
+++ RailDispatchMono/RailDispatchMono.Core/Game/Train/Vehicle.cs
@@ -8,18 +8,21 @@ public abstract class Vehicle
 
     public VehicleParameters Parameters { get; }
 
+    /// <summary>
+    /// Static coupling-interface data for the rolling-stock definition.
+    /// Connection state is not stored on the vehicle yet; 0.1.5 will own that state.
+    /// </summary>
+    public CouplingSpecification Coupling { get; }
+
     public VehicleOrientation Orientation { get; set; }
 
     protected Vehicle(
-        VehicleParameters parameters)
+        VehicleParameters parameters,
+        CouplingSpecification? coupling = null)
     {
-        Id =
-            Guid.NewGuid();
-
-        Parameters =
-            parameters;
-
-        Orientation =
-            VehicleOrientation.Forward;
+        Id = Guid.NewGuid();
+        Parameters = parameters;
+        Coupling = coupling ?? CouplingSpecification.Default;
+        Orientation = VehicleOrientation.Forward;
     }
 }
