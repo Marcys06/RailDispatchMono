2ac51068a23cac97698b742997359cdb84d1bb8e
Marcys06 <marcelcieslik6@gmail.com>
2026-09-06 19:50:49 +0200

    fix(0.2.5): tidy railway save DTO

--- RailDispatchMono/RailDispatchMono.Core/Game/Save/MapSaveData.cs
+++ RailDispatchMono/RailDispatchMono.Core/Game/Save/MapSaveData.cs
@@ -53,8 +53,7 @@ public sealed class MapPositionSaveData
 public sealed class SignalSaveData
 {
     public Guid Id { get; set; }
-    public string Name { get; set; } = ""
-    ;
+    public string Name { get; set; } = "";
     public int X { get; set; }
     public int Y { get; set; }
     public TrackConnections Direction { get; set; }
